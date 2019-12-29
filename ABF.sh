d14be710b58bfe0b0bd85010b8257a8ca759f41e
Mikhail Novosyolov <m.novosyolov@rosalinux.ru>
2019-12-29 15:54:03 +0300

    try to workaround freezes of urpmi and/or RPM DB deadlocks

--- ABF.sh
+++ ABF.sh
@@ -5,7 +5,12 @@ export outDir="/home/vagrant/results"
 export rootfsXzCompressLevel=9
 [ ! -d "$outDir" ] && mkdir -p "$outDir"
 
-urpmi coreutils findutils sed tar urpmi util-linux squashfs-tools xz --auto
+# Attach a repo with newer urpmi and perl-URPM which seem to not freeze randomly
+# They are already in rosa2019.1 and rosa2019.1, currently in a separate repo for rosa2016.1
+if [ "$(rpm --eval '%mdvver')" = 201610 ]; then
+	urpmi.addmedia corp_test_personal "http://abf-downloads.rosalinux.ru/corp_test_personal/repository/rosa2016.1/$(rpm --eval '%_arch')/main/release"
+fi
+urpmi coreutils findutils sed tar urpmi perl-URPM util-linux squashfs-tools xz --auto
 
 if [ "$BUILD_MAIN_IMAGE_ONLY" = 1 ]
 	then ./mkimage-urpmi.sh
