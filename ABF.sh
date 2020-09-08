46800579a8a0476755a0c9ae23c3c77608e4dfcc
Mikhail Novosyolov <m.novosyolov@rosalinux.ru>
2020-09-08 12:31:48 +0300

    add script for dnf (rosa2019.1)

--- ABF.sh
+++ ABF.sh
@@ -5,14 +5,21 @@ export outDir="/home/vagrant/results"
 export rootfsXzCompressLevel=9
 [ ! -d "$outDir" ] && mkdir -p "$outDir"
 
-# Attach a repo with newer urpmi and perl-URPM which seem to not freeze randomly
-# They are already in rosa2019.1 and rosa2019.1, currently in a separate repo for rosa2016.1
-if [ "$(rpm --eval '%mdvver')" = 201610 ]; then
-	urpmi.addmedia corp_test_personal "http://abf-downloads.rosalinux.ru/corp_test_personal/repository/rosa2016.1/$(rpm --eval '%_arch')/main/release"
+if ! command -v dnf 2>/dev/null >/dev/null
+	then
+		TYPE=urpmi
+		urpmi --auto-update --auto --auto-select
+		urpmi coreutils findutils sed tar urpmi perl-URPM util-linux squashfs-tools xz --auto
+	else
+		TYPE=dnf
+		# temp
+		BUILD_MAIN_IMAGE_ONLY=1
+		dnf refresh
+		dnf distrosync -y
+		dnf install -y coreutils findutils sed tar util-linux squashfs-tools xz
 fi
-urpmi coreutils findutils sed tar urpmi perl-URPM util-linux squashfs-tools xz --auto
 
 if [ "$BUILD_MAIN_IMAGE_ONLY" = 1 ]
-	then ./mkimage-urpmi.sh
+	then ./mkimage-${TYPE}.sh
 	else ./build-all.sh
 fi
