e9da1dc14283c8e1720350d70d186af5bd816ead
fedya <alexander@mezon.ru>
2021-10-05 09:43:08 +0000

    Updated mkimage-minimal.sh

--- mkimage-minimal.sh
+++ mkimage-minimal.sh
@@ -2,6 +2,6 @@
 # suitable for rosa2014.1 and above
 
 export imgType="min"
-export packagesList="basesystem-minimal urpmi bash vim-minimal nano sudo termcap initscripts systemd branding-configs-fresh"
+export packagesList="basesystem-minimal urpmi bash vim-minimal nano sudo termcap initscripts systemd branding-configs-fresh tar"
 
 ./mkimage-urpmi.sh
